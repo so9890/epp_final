57f696a1544d5e2cd4e9e52ead3cac114c06ca9e
Dobkowitz <sdobkowitz@neori.uni-bonn.de>
2019-03-06 15:08:46 +0100

    Problem with module function in sphinx. changed conf.py

--- src/data_management/data_functions/test_CEX_DATA.py
+++ src/data_management/data_functions/test_CEX_DATA.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import pytest
 
-from data_function.functions import _cum_distribution, weights_percentiles
+from functions import _cum_distribution, weights_percentiles
 from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 
